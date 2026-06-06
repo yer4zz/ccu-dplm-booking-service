06d5d8317173de98647d8c4cd9a10299c9b81911
yerazz <work.yer4ss@gmail.com>
2026-06-06 11:20:47 +0500

    fix api url fallback

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api/v1',    //http://localhost:8080/api/v1
+  baseURL: import.meta.env.VITE_API_URL ?? 'https://ccu-dplm-booking-service.onrender.com/api/v1',  //import.meta.env.VITE_API_URL || '/api/v1',    //http://localhost:8080/api/v1
   headers: { 'Content-Type': 'application/json' },
 })
 
